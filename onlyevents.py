189512f20126e5be2d9e74946e1cbf37a301e4a9
xlab992 <demon_raz92@hotmail.it>
2025-05-23 22:37:36 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -9,7 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-PRXLINK = "" # non mettere lo / finale
+PRXLINK = "https://xlab992-ddl.hf.space" # non mettere lo / finale
 
 # Costanti
 NUM_CHANNELS = 10000
