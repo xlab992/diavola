657af7b0cfd54b379c6c02b971d87c2fcc18c5aa
xlab992 <demon_raz92@hotmail.it>
2025-05-23 22:35:30 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-PRXLINK = "" # non mettere lo / finale
+PRXLINK = "https://xlab992-ddl.hf.space" # non mettere lo / finale
 
 
 # Costanti
