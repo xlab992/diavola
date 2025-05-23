76887a5793a594e27edfc2fc92685e1bfe9ddf42
xlab992 <demon_raz92@hotmail.it>
2025-05-23 22:39:27 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -3,10 +3,10 @@ import json
 import re
 import os
 
-USREPG = ""
-BRANCHEPG = ""
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+USREPG = "xlab992"
+BRANCHEPG = "diavola"
+MFPLINK = "https://xlab992-bb.hf.space"     # non mettere lo / finale al link
+MFPPSW = "gf4"
 
 
 PROXY = f"{MFPLINK}/proxy/hls/manifest.m3u8?api_password={MFPPSW}&d="
