702a4b65722cb76527b279bda2a9c4e80c9bf728
elwood65 <fenotti65@gmail.com>
2025-05-25 08:01:21 -0300

    Update hat.py

--- hat.py
+++ hat.py
@@ -10,7 +10,7 @@ import time
 import html
 from urllib.parse import urljoin
 
-MFPLINK = "LINKMFP"
+MFPLINK = "https://elwood65-vadase.hf.space"
 
 # Funzioni dal tuo mpd_decoder.py
 def extract_mpd_link_from_page(url):
