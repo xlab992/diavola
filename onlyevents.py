c4c8a8a94458d1b898601196dea7b55032e8a47f
xlab992 <demon_raz92@hotmail.it>
2025-05-28 14:28:13 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -9,7 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-PRXLINK = "http://nsaxdhyv-rotate:x6jqsk51dg8y@p.webshare.io:80" 
+PRXLINK = "https://xlab992-ddl.hf.space" 
 
 
 # Costanti
