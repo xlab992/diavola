e067a25292f3fa044a452f0a095af067ee1dc36a
xlab992 <demon_raz92@hotmail.it>
2025-05-28 14:28:39 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -12,7 +12,7 @@ import time
 import re
 from urllib.parse import quote_plus  # Add this import
 
-PRXLINK ="http://nsaxdhyv-rotate:x6jqsk51dg8y@p.webshare.io:80" 
+PRXLINK ="https://xlab992-ddl.hf.space" 
 
 
 # Constants
