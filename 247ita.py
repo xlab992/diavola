30b4c0ea07b64757bfe97bde95c02b2ec60710a4
xlab992 <demon_raz92@hotmail.it>
2025-05-28 14:29:25 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-PRXLINK = "http://nsaxdhyv-rotate:x6jqsk51dg8y@p.webshare.io:80" 
+PRXLINK = "https://xlab992-ddl.hf.space" 
 
 # Costanti
 M3U8_OUTPUT_FILE = "247ita.m3u8"
